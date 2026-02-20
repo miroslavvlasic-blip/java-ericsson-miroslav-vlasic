6c8ad86fb7a1733663f7a2f9f88cb64f6f17635a
emirvla <miroslav.vlasic@gmail.com>
2026-02-20 08:55:52 +0100

    Ucilica

---
+++ Java ENT/src/Ucilica.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Ucilica {
+
+    private List<GeometrijskiLik> likovi = new ArrayList<>();
+
+    public void dodaj(GeometrijskiLik lik) {
+        likovi.add(lik);
+    }
+
+    public void ispisiSve() {
+        for (GeometrijskiLik lik : likovi) {
+            System.out.println(lik);
+        }
+    }
+
+    public void sortirajPoPovrsini() {
+        Collections.sort(likovi);
+    }
+}
