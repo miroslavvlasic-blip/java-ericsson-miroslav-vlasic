a0936c5fc226156cbd0f56ed81ff9015fb8600b1
emirvla <miroslav.vlasic@gmail.com>
2026-02-20 08:54:15 +0100

    Krug

---
+++ Java ENT/src/Krug.java
@@ -0,0 +1,20 @@
+
+public class Krug extends GeometrijskiLik {
+
+    private double r;
+
+    public Krug(String naziv, double r) {
+        super(naziv);
+        this.r = r;
+    }
+
+    @Override
+    public double povrsina() {
+        return Math.PI * r * r;
+    }
+
+    @Override
+    public double opseg() {
+        return 2 * Math.PI * r;
+    }
+}
